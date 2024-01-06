89e9b065c644ac1f375db323293d1b35b7a28afa
Nik P <niaher.crimston@gmail.com>
2024-01-06 14:06:03 +0700

    chore: remove unused internal class

--- Filer.EntityFrameworkCore/DbEntityConfiguration.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Filer.EntityFrameworkCore
-{
-	using Microsoft.EntityFrameworkCore.Metadata.Builders;
-
-	internal abstract class DbEntityConfiguration<TEntity> where TEntity : class
-	{
-		public abstract void Configure(EntityTypeBuilder<TEntity> entity);
-	}
-}
